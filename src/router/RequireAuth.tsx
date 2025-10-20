db0d20bf5ae243e2443f7114a9871ab40a025d01
bmoreno@intelutions.net <bmoreno@intelutions.net>
2025-10-20 14:51:28 -0400

    fix

--- src/router/RequireAuth.tsx
+++ src/router/RequireAuth.tsx
@@ -1,7 +1,7 @@
 // src/router/RequireAuth.tsx
 import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
-import { useAuth } from '../context/useAuth'; // 👈 ya lo tienes separado
+import { useAuth } from '../context/useAuth'; // ya lo tienes separado
 
 const RequireAuth: React.FC = () => {
   const { token } = useAuth();
